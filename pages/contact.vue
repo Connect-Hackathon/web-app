802dae7129bd4e1a9b90c9f0f8592939f11aad24
Harrison Ratcliffe <58830505+Strootle@users.noreply.github.com>
2023-01-27 15:41:52 +0000

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,9 +1,21 @@
 <template>
     <div>
-        <p>admin area</p>
+        <h1>How to contact us...</h1>
         <div class="container row-1">
-            <div v-for="people of peeps" :key="people.name">
-                <p>{{ people.name }}</p>
+            <br>
+            <div>
+                <h3>Telephone</h3>
+                <p>To contact us via telephone, please contact 0800 123 8911. Please be advised that our opening hours are 9am - 6pm Monday to Friday
+                    and from 10am - 3pm on weekends. Phone calls are recorded for protection.
+                </p>
+                <h3>Email</h3>
+                <p>To reach us via email. Send an email to "support@c5.com" and will be responded to within 2 standard working days. Within your email please include the following...</p>
+                <ul>
+                    <li>Name and contact information</li>
+                    <li>Reason for contacting</li>
+                    <li>Date of occured problem</li>
+                    <li>Reference number of issue (if applicable)</li>
+                </ul>
             </div>
             
         </div>
