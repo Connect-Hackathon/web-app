0c7d1886078dc5ace4e39ed2924020056869fbe5
Palvir <59867306+palzino@users.noreply.github.com>
2023-01-27 13:16:11 +0000

    delete and replace

--- matches/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . import models
-
-# Register your models here.
-admin.site.register(models.Doubles)
-admin.site.register(models.MixedDoubles)
-admin.site.register(models.DoublesWithNoPartner)
-admin.site.register(models.Singles)
--- readme.md
+++
@@ -1,3 +0,0 @@
-# Migrate to the DB
-
-`python manage.py makemigrations` then `python manage.py migrate`
